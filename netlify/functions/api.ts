023004c6f68ead9751903626dfe818264cdae713
Prafulla Joshi <pjoshi.dev@gmail.com>
2023-09-21 21:43:09 +0530

    Netlify setup

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -6,7 +6,7 @@ const api = express();
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/api/", router);
+// api.use("/api/", router);
 api.use("/.netlify/functions/", router);
 
 export const handler = serverless(api);
