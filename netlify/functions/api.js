5039515c9820c9e8793be7970ee2e9a6dc40a50a
Prafulla Joshi <pjoshi.dev@gmail.com>
2023-09-23 19:05:29 +0530

    Exporting API handler

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -26,5 +26,4 @@ router.get("/hello", (req, res) => res.send("Hello World!"));
 // api.use("/api/", router);
 api.use("/.netlify/functions/api", router);
 
-// export const handler = serverless(api);
-module.exports = serverless(api);
+export const handler = serverless(api);
